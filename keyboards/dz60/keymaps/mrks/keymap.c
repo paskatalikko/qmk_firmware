71effd005d966b0dac361a773f796c87fd3ea84d
Markus Karppinen <paskatalikko@hotmail.com>
2021-06-26 20:28:10 +0300

    Added DZ60 layout for 7U bottom row and split rshift

--- keyboards/dz60/keymaps/mrks/keymap.c
+++ keyboards/dz60/keymaps/mrks/keymap.c
@@ -73,25 +73,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |Ctrl |GUI|Alt  |         Space         |Alt  |GUI|_CN|Ctrl |
    * `-----------------------------------------------------------'
    */
-  [_QWERTY] = LAYOUT_60_iso(
+  [_QWERTY] = LAYOUT_60_iso_mrks(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
     MO(_FN), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,
-    KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RGUI, MO(_CN),   KC_RCTL),
+    KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(_CN),
+    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                                      KC_RALT, KC_RGUI, KC_RCTL),
 
-  [_FUNCTION] = LAYOUT_60_iso(
+  [_FUNCTION] = LAYOUT_60_iso_mrks(
     KC_GRV,  KC_F1,     KC_F2,   KC_F3,   KC_F4,          KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_DEL,
     XXXXXXX, SYSTEMOUT, XXXXXXX, XXXXXXX, RESTARTSERVER1, XXXXXXX, XXXXXXX, KC_PGUP, KC_UP,   KC_PGDN,  KC_PSCR, XXXXXXX, XXXXXXX,
     _______, XXXXXXX,   SOLRLOG, REXPATH, XXXXXXX,        XXXXXXX, KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-    _______, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, KC_END,  XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, _______,
-    _______, _______,   _______,                          XXXXXXX,                             _______, _______, _______, _______),
+    _______, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, KC_END,  XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, _______, XXXXXXX,
+    _______, _______,   _______,                          XXXXXXX,                                      _______, _______, _______),
 
-  [_CONTROL] = LAYOUT_60_iso(
+  [_CONTROL] = LAYOUT_60_iso_mrks(
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-    _______, _______, _______,                            RGB_TOG,                            _______, _______, _______, _______)
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX,
+    _______, _______, _______,                            RGB_TOG,                                     _______, _______, _______)
 
 };
